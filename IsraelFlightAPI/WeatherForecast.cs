d8f27a6c5d0695d4188141a22518a3d00dec86d4
Miryam Peretz <miri2451@gmail.com>
2024-09-23 19:57:24 +0300

    bla

--- IsraelFlightAPI/WeatherForecast.cs
+++ IsraelFlightAPI/WeatherForecast.cs
@@ -1,3 +1,4 @@
+// miryam 
 namespace IsraelFlightAPI
 {
     public class WeatherForecast
@@ -8,6 +9,7 @@ namespace IsraelFlightAPI
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string? Summary { get; set; 
+        }
     }
 }
